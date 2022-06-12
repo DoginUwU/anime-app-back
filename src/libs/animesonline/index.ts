fc7ddd4a81f27ec1c85f38985de490fdbe87796c
Luiz Eduardo <doginuwu@gmail.com>
2022-06-12 18:01:41 -0300

    .

--- src/libs/animesonline/index.ts
+++ src/libs/animesonline/index.ts
@@ -9,7 +9,9 @@ import { Engine } from "../engine";
 
 class AnimesOnline extends Engine {
     headers: AxiosRequestHeaders = {
-        'origin': 'https://animesonline.cc'
+        'origin': 'https://animesonline.cc',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36',
+        'accept': '*/*',
     };
 
     constructor() {
