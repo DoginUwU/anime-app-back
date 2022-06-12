4fedec986445e4cabc98511966278cb665580bde
Luiz Eduardo <doginuwu@gmail.com>
2022-06-12 16:01:00 -0300

    add error handling

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -1,7 +1,21 @@
 import axios from "axios";
+import AppError from "../errors/AppError";
 
 const api = axios.create({
     baseURL: "https://still-brushlands-24168.herokuapp.com/",
 });
+
+//intercep 403 error
+api.interceptors.response.use(
+    (response) => {
+        return response;
+    }
+    , (error) => {
+        if (error.response.status === 403) {
+            throw new AppError(error.response.data.message, 403);
+        }
+        return Promise.reject(error);
+    }
+);
 
 export { api };
