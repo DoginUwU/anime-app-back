a1c311a2d002880916b02496ed8fca3654c8c770
Luiz Eduardo <doginuwu@gmail.com>
2022-06-12 18:27:29 -0300

    a

--- src/libs/axios.ts
+++ src/libs/axios.ts
@@ -2,7 +2,6 @@ import axios from "axios";
 import AppError from "../errors/AppError";
 
 const api = axios.create({
-    baseURL: 'https://still-brushlands-24168.herokuapp.com/',
     timeout: 10000,
 });
 
