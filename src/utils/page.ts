cd51f07111054e947a7949dc4a124d63459229fe
Luiz Eduardo <doginuwu@gmail.com>
2022-06-12 17:03:52 -0300

    .

--- src/utils/page.ts
+++ src/utils/page.ts
@@ -1,31 +1,19 @@
 import { AxiosRequestHeaders } from "axios";
 import { CheerioAPI, load } from "cheerio";
 import { api } from "../libs/axios";
-import puppeteer from "puppeteer";
 
 const getPage = async (
   url: string,
   params: string,
   headers?: AxiosRequestHeaders
 ): Promise<CheerioAPI> => {
-  // return api
-  //   .get(`${url}/${params}`, {
-  //     headers,
-  //   })
-  //   .then((response) => {
-  //     return load(response.data);
-  //   });
-  const browser = await puppeteer.launch({
-    'args' : [
-      '--no-sandbox',
-      '--disable-setuid-sandbox'
-    ]
-  });
-  const page = await browser.newPage();
-  await page.goto(`${url}/${params}`);
-
-  const html = await page.content();
-  return load(html);
+  return api
+    .get(`${url}/${params}`, {
+      headers,
+    })
+    .then((response) => {
+      return load(response.data);
+    });
 };
 
 export { getPage };
